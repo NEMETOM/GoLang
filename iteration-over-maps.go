85e2bf11d4a81eea4398edc2c16178a0baaae3e7
NEMETOM <tom.nemeth85@gmail.com>
2020-11-10 00:57:13 +0000

    Create iteration-over-maps.go

---
+++ iteration-over-maps.go
@@ -0,0 +1,22 @@
+//
+// This code iterates over maps and prints HEX code for each color defined in the map
+//
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":"#ff0000",
+		"green":"#4bf745",
+		"white":"#ffffff",
+	}
+	
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
